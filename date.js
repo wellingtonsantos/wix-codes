8795fefd7d5de26422cc6685539fd32f6e58c753
wellingtonsantos <ppelanca@gmail.com>
2022-02-09 19:59:26 -0300

    descrição date.js

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
-// PEAGR APENAS O TEMPO DA DATA
+// PEGAR APENAS O TEMPO DA DATA
+// Use a variavel time para ter o resultado
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-console.log(time)
